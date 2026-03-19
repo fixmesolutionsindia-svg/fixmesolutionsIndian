e0be0943e7a69c3b8acbfd21f8416563b9165937
fixmesolutionsindia-svg <fixmesolutionsindia@gmail.com>
2026-03-20 02:05:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ function toggleMenu(){
 const menu = document.getElementById("mobileMenu");
 menu.classList.toggle("active");
 }
+function sendBookingToWhatsApp() {
+
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var phone = document.getElementById("phone").value;
+var device = document.getElementById("device").value;
+var brand = document.getElementById("brand").value;
+var service = document.getElementById("service").value;
+var location = document.getElementById("location").value;
+var slot = document.getElementById("slot").value;
+var problem = document.getElementById("problem").value;
+
+var message = "Hello FixMe Solutions, I want to book a repair service.%0A%0A"
++ "Name: " + name + "%0A"
++ "Phone: " + phone + "%0
